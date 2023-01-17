71e57b290fb11546103a91ee1201c12a49738a0a
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2023-01-17 13:24:42 +0100

    try to disable the gnome-keyring connection

--- src/utils.js
+++ src/utils.js
@@ -69,6 +69,8 @@ const getCommonPuppArgs = (extras = []) =>
     '--disable-crash-reporter',
     '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0)"',
     '--use-gl=egl',
+    '--password-store=basic',
+    '--use-mock-keychain',
     ...extras,
   ].filter((x) => x);
 
